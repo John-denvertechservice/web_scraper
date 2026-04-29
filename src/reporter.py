fe6cd4df12d505546b60d14362eae7253b0430c2
user <user@users-MacBook-Pro-2.local>
2026-04-29 13:37:28 -0600

    reporter.py | Parses JSON file, formats webscrape raw data into a readable format.

--- src/reporter.py
+++ src/reporter.py
@@ -2,4 +2,30 @@ import pandas as pd
 import glob
 import os
 
-def get_latest_scrape
+def get_latest_scrape():
+    list_of_files = glob.glob('data/raw/*.json')
+    if not list_of_files:
+        return None
+    return max(list_of_files, key=os.path.getctime)
+
+def generate_summary():
+    latest_file = get_latest_scrape()
+    if not latest_file:
+        print("No data files found to report on.")
+        return
+    print(f"Generating report from {latest_file}")
+    df = pd.read_json(latest_file)
+    df['price_clean'] = df['price'].str.replace('Â£', '').str.replace('£', '').astype(float)
+
+    total_books = len(df)
+    avg_price = df['price_clean'].mean()
+    most_expensive = df.loc[df['price'].idxmax()]
+    print("-" * 30)
+    print(f"DAILY BOOK REPORT")
+    print("-" * 30)
+    print(f"Total Books Scraped: {total_books}")
+    print(f"Average Price: £{avg_price:.2f}")
+    print(f"Most Expensive: {most_expensive['title']} (£{most_expensive['price_clean']})")
+    print("-" * 30)
+if __name__ ==  "__main__":
+    generate_summary()
